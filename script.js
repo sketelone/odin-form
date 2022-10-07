a234394a306a729af966cce28698224b980c90be
Sabrina <sketelone@gmail.com>
2022-10-07 10:52:16 -0700

    Fix CSS validation styles

--- script.js
+++ script.js
@@ -5,24 +5,39 @@ and */
 
 
 //create constants for referring to html elements
-const passsword = document.getElementById('password');
+const submit = document.getElementById('submit');
+const password = document.getElementById('password');
 const confirm_password = document.getElementById('confirm-password');
 
 //when user inputs confirm password, check if it matches
-confirm_password.addEventListener("input", function(event) {
-    if (passwordMatch(password, "input") == false) {
-        confirm_password.setCustomValidity("Passwords do not match!");
-        confirm_password.reportValidity();
+submit.addEventListener("click",function(event) {
+    var pass = password.value;
+    var conPass = confirm_password.value;
+    if (passwordMatch(pass,conPass) == false) {
+            console.log(pass,conPass)
+            confirm_password.setCustomValidity("Passwords do not match!");
+            confirm_password.reportValidity();
     } else {
         confirm_password.setCustomValidity("");
     }
 })
 
+// confirm_password.addEventListener("input", function(event) {
+//     if (passwordMatch(password, "input") == false) {
+//         console.log("input")
+//         confirm_password.setCustomValidity("Passwords do not match!");
+//         confirm_password.reportValidity();
+//     } else {
+//         confirm_password.setCustomValidity("");
+//     }
+// })
+
 // validate form input
-function passwordMatch(a,b) {
+function passwordMatch(a, b) {
     console.log("password match", a, b)
     if (a != b) {
         return(false);
+        console.log()
     } else {
         return(true);
     }
